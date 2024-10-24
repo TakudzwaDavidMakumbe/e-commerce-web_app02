68b1370f3e9268ae37b51dedd59a603a17595ffa
TakudzwaDavidMakumbe <takuclarity@gmail.com>
2024-10-24 09:45:15 +0200

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className='border-t border-gray-300 mt-16'>
         <div className="container text-center text-gray-500 py-4">
-            &copy; 2024 - All rights reserved to <a href="https://www.facebook.com/" target="_blank" rel="noreferrer" className="text-blue-500">Takudzwa Makumbe</a>.
+            &copy; 2024 - All rights reserved to <a href="https://github.com/TakudzwaDavidMakumbe/e-commerce-web_app02/" target="_blank" rel="noreferrer" className="text-blue-500">Takudzwa Makumbe</a>.
         </div>
       
     </div>
